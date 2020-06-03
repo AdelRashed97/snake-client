8d692e4948ec0a94abd1b0225a16f5523f79620b
Adel Rashed <adel_rashed@outlook.com>
2020-06-03 20:43:23 +0000

    added a connection event handler

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect',() => console.log("Successfuly connected to the game server"));
 
   conn.on('data',(data) => console.log(data));
 
