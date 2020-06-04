9523e2f749425e28efcd46845af18558efb07b53
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 01:21:58 +0000

    modified connect function to accept IP and PORT as parameters

--- client.js
+++ client.js
@@ -3,10 +3,10 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
+const connect = function(IP,PORT) {
   const conn = net.createConnection({
-    host: "135.23.222.148",
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
