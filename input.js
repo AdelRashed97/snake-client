d5b80434a2eeebdb90c91a07bd66831a88dce5ce
Adel Rashed <adel_rashed@outlook.com>
2020-06-04 00:13:59 +0000

    updated handle function to accept movements

--- input.js
+++ input.js
@@ -2,6 +2,21 @@ const handleUserInput = (key) =>{
   if (key === '\u0003') {
     console.log("Left Game")
     process.exit();
+  } else if ( key ==='\x77') {
+    // key = w
+    console.log("up");
+
+  } else if (key === '\x61') {
+    // key = a
+    console.log('left');
+
+  } else if (key === '\x73') {
+    // key =s
+    console.log('down');
+
+  } else if (key === '\x64') {
+    // key = d
+    console.log('right');
   }
 };
 const setupInput = function() {
@@ -13,4 +28,5 @@ const setupInput = function() {
   return stdin;
 };
 
+setupInput();
 module.exports = {setupInput};
